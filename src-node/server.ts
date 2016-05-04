98f6f797e26b1600bb81bc67587848827c6cd8de
danelkhen <danelkhen@gmail.com>
2016-05-04 14:13:43 +0200

    cleanups and file structuring

--- src-node/server.ts
+++ src-node/server.ts
@@ -4,13 +4,31 @@ import * as path from 'path';
 var app = express();
 
 let rootDir = path.join(__dirname, "../../..")
-app.use("/res", express.static(rootDir));
+app.use("/res", express.static(rootDir), (req, res) => {
+//    res.status(404);
+    res.sendStatus(404);
+
+    //// respond with html page
+    //if (req.accepts('html')) {
+    //    res.render('404', { url: req.url });
+    //    return;
+    //}
+
+    //// respond with json
+    //if (req.accepts('json')) {
+    //    res.send({ error: 'Not found' });
+    //    return;
+    //}
+
+    //// default to plain-text. send()
+    //res.type('txt').send('Not found');
+});
 
 app.get("*", function (req, res) {
     //if (req.params.base == "res")
     //    throw new Error();
 
-    let file = path.join(path.join(rootDir, "test/index2.html"));
+    let file = path.join(path.join(rootDir, "viewer/index.html"));
     console.log({ base: req.params.base, url: req.url });
     console.log("sending", file);
     res.sendFile(file);
--- viewer/test/index.ts
+++ viewer/test/index.ts
@@ -1,11 +1,11 @@
-﻿/// <reference path="../src/extensions.ts" />
+﻿/// <reference path="../../src/extensions.ts" />
 "use strict";
 
-import {Token, TokenType, File2, } from "../src/token";
-import {AstWriter} from "../src/ast-writer";
-import {ParserBase} from "../src/parser-base";
-import {ExpressionParser} from "../src/expression-parser";
-import {Parser} from "../src/parser";
+import {Token, TokenType, File2, } from "../../src/token";
+import {AstWriter} from "../../src/ast-writer";
+import {ParserBase} from "../../src/parser-base";
+import {ExpressionParser} from "../../src/expression-parser";
+import {Parser} from "../../src/parser";
 import {
 AstNode, Expression, Statement, UnresolvedExpression, SimpleName, SubroutineDeclaration, SubroutineExpression, ArrayMemberAccessExpression, ArrayRefDeclaration,
 BarewordExpression, BeginStatement, BinaryExpression, Block, BlockExpression, BlockStatement, ElseStatement, ElsifStatement, EmptyStatement, EndStatement,
@@ -14,14 +14,14 @@ NamedMemberExpression, NativeFunctionInvocation, NativeInvocation_BlockAndListOr
 Operator, PackageDeclaration, ParenthesizedList, PostfixUnaryExpression, PrefixUnaryExpression, QwExpression, RawExpression, RawStatement, RegexExpression,
 ReturnExpression, TrinaryExpression, Unit, UnlessStatement, UseOrNoStatement, UseStatement, ValueExpression, VariableDeclarationExpression, VariableDeclarationStatement, WhileStatement,
 HasArrow, HasLabel,
-} from "../src/ast";
-import {PrecedenceResolver} from "../src/precedence-resolver";
-import {TokenTypes} from "../src/token-types";
-import {Tokenizer} from "../src/tokenizer";
-import {safeTry, TokenReader, Logger, AstNodeFixator} from "../src/utils";
-import "../src/extensions";
-import {RefArrayToRefUtil} from "../src/refactor";
-import {ExpressionTester, EtReport, EtItem} from "../src/expression-tester";
+} from "../../src/ast";
+import {PrecedenceResolver} from "../../src/precedence-resolver";
+import {TokenTypes} from "../../src/token-types";
+import {Tokenizer} from "../../src/tokenizer";
+import {safeTry, TokenReader, Logger, AstNodeFixator} from "../../src/utils";
+import "../../src/extensions";
+import {RefArrayToRefUtil} from "../../src/refactor";
+import {ExpressionTester, EtReport, EtItem} from "../../src/expression-tester";
 
 export class IndexPage {
 
